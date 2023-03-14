cf75237312a5b87212ad062bd0d3c9d6ce41f27a
KaMZeSs <83509865+KaMZeSs@users.noreply.github.com>
2023-03-14 09:25:51 +0300

    Добавьте файлы проекта.

---
+++ DataBase_Medical/HospitalStaff.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBase_Medical
+{
+    public class HospitalStaff // Класс для примера интерфейса
+    {
+        //public int id;
+        public string Имя { get; set; }
+        public string Фамилия { get; set; }
+        public string Отчество { get; set; }
+        public string Отделение { get; set; }
+        public string Категория { get; set; }
+        public string Дата_принятия_на_работу { get; set; }
+        public string Зарплата { get; set; }
+    }
+}
